d0d8822494f8b7a08d230d8a4a129b2143e8d285
tercel.yi <tercel.yi@gmail.com>
2011-04-15 13:27:23 +0000

    import code

---
+++ android/ChildDev/src/com/raral/base/ChildDevBaseTest.java
@@ -0,0 +1,74 @@
+package com.raral.childdev.base;
+
+import java.util.ArrayList;
+
+import org.cocos2d.layers.CCScene;
+
+import com.raral.childdev.TestBundle;
+
+abstract public class ChildDevBaseTest {
+	protected TestBundle bundle = null;
+	private int _nextI = 0;
+	public ArrayList<CCScene> _scenes = null;
+	public int score = 0;
+	public boolean tested = false;
+	
+	public boolean isTested() {
+		return tested;
+	}
+
+	public ChildDevBaseTest(TestBundle bundle) {
+		super();
+		this.bundle = bundle;
+	}
+
+	public TestBundle getBundle() {
+		return bundle;
+	}
+
+	public void setBundle(TestBundle bundle) {
+		this.bundle = bundle;
+	}
+
+	public int getScore() {
+		if (score < 0)
+			return 0;
+		else
+			return score;
+	}
+
+	abstract public String getName();
+
+	abstract public String getDesc();
+
+	abstract public ArrayList<CCScene> loadScenes();
+
+	public void load() {
+		_scenes = loadScenes();
+		tested = true;
+	}
+
+	public boolean hasNextScene() {
+		if (_nextI < _scenes.size()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public CCScene nextScene() {
+		return _scenes.get(_nextI++);
+	}
+
+	public void resetScene() {
+		_nextI = 0;
+	}
+
+	public void onSceneFinish() {
+		// Override to impl
+	}
+
+	public void onTestFinish() {
+		// Override to impl
+	}
+}
