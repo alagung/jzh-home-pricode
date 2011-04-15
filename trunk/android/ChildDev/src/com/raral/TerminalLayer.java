3a1aabf19df3e01252c390f1da5072da8275bd19
tercel.yi@gmail.com <tercel.yi@gmail.com@bba6d69d-a2d5-1cfd-af19-a508fabc1c1c>
2011-04-15 13:27:23 +0000

    import code

---
+++ trunk/android/ChildDev/src/com/raral/TerminalLayer.java
@@ -0,0 +1,68 @@
+package com.raral.childdev;
+
+import org.cocos2d.actions.base.CCRepeatForever;
+import org.cocos2d.actions.interval.CCAnimate;
+import org.cocos2d.nodes.CCAnimation;
+import org.cocos2d.nodes.CCDirector;
+import org.cocos2d.nodes.CCLabel;
+import org.cocos2d.nodes.CCSprite;
+import org.cocos2d.types.CGSize;
+import org.cocos2d.utils.CCFormatter;
+
+import android.view.MotionEvent;
+
+import com.raral.childdev.base.ChildDevBaseLayer;
+import com.raral.childdev.base.ChildDevBaseTest;
+
+public class TerminalLayer extends ChildDevBaseLayer {
+	TestBundle _bundle;
+
+	public TerminalLayer(TestBundle bundle) {
+		super(null);
+		isTouchEnabled_ = true;
+		_bundle = bundle;
+
+		ChildDevBaseTest tests[] = _bundle.getTests();
+		
+		CGSize s = CCDirector.sharedDirector().winSize();
+		float yy = s.height - 20;
+		for (int i=0;i<tests.length;++i)
+		{
+			ChildDevBaseTest t = tests[i];
+			CCLabel line = CCLabel.makeLabel(CCFormatter.format("%s: %4d", t
+					.getName(), t.getScore()), "DroidSans", 16);
+			line.setAnchorPoint(0.5f, 1.0f);
+	        line.setPosition(s.width / 2, yy);
+	        addChild(line);
+	        yy = line.getPosition().y - line.getContentSizeRef().height - 20;	        
+		}
+		CCLabel score = CCLabel.makeLabel(CCFormatter.format("×ÜµÃ·Ö: %d", bundle
+				.getTotalScore()), "DroidSans", 36);
+		score.setAnchorPoint(0.5f, 1.0f);
+		score.setPosition(s.width / 2, yy);
+		addChild(score);
+		yy = score.getPosition().y - score.getContentSizeRef().height - 20;	
+		
+		CCAnimation animation = CCAnimation.animation("report", 0.2f);
+        for (int i = 1; i < 10; i++) {
+            animation.addFrame(CCFormatter.format("grossini_dance_%02d.png", i));
+        }
+        
+        CCSprite report = new CCSprite("grossini.png");
+        report.setAnchorPoint(0.5f, 1.0f);
+        report.setPosition(s.width / 2, yy);
+		addSimpleAct(1.0f, report, CCRepeatForever.action(CCAnimate.action(animation)));
+	}
+
+	@Override
+	public boolean ccTouchesEnded(MotionEvent event) {
+		_bundle.postToUI(new Runnable() {			
+			@Override
+			public void run() {
+				CCDirector.sharedDirector().getActivity().finish();
+			}
+		});
+		
+		return true;
+	}
+}
---
+++ trunk/android/ChildDev/src/com/raral/base/NodeEventLayer.java
@@ -0,0 +1,128 @@
+package com.raral.childdev.base;
+
+import org.cocos2d.events.CCTouchDispatcher;
+import org.cocos2d.layers.CCLayer;
+import org.cocos2d.menus.CCMenuItem;
+import org.cocos2d.nodes.CCNode;
+import org.cocos2d.protocols.CCTouchDelegateProtocol;
+
+import android.view.MotionEvent;
+
+public abstract class NodeEventLayer extends ChildDevBaseLayer{
+	public NodeEventLayer(ChildDevBaseTest test) {
+		super(test);
+		isTouchEnabled_ = true;
+	}
+	
+	private boolean itemEventImpl(MotionEvent event, CCNode item) {
+		int action = event.getAction();
+		int actionCode = action & MotionEvent.ACTION_MASK;
+		
+		switch (actionCode) {
+		case MotionEvent.ACTION_DOWN:
+		case MotionEvent.ACTION_POINTER_DOWN:
+			if (item instanceof CCTouchDelegateProtocol)
+				return ((CCTouchDelegateProtocol)item).ccTouchesBegan(event);
+			else
+				return ccTouchesBeganPro(event, item);
+		case MotionEvent.ACTION_CANCEL:
+			if (item instanceof CCTouchDelegateProtocol)
+				return ((CCTouchDelegateProtocol)item).ccTouchesCancelled(event);
+			else
+				return ccTouchesCancelledPro(event, item);
+		case MotionEvent.ACTION_MOVE:
+			if (item instanceof CCTouchDelegateProtocol)
+				return ((CCTouchDelegateProtocol)item).ccTouchesMoved(event);
+			else
+				return ccTouchesMovedPro(event, item);
+		case MotionEvent.ACTION_UP:
+		case MotionEvent.ACTION_POINTER_UP:
+			if (item instanceof CCTouchDelegateProtocol)
+				return ((CCTouchDelegateProtocol)item).ccTouchesEnded(event);
+			else
+				return ccTouchesEndedPro(event, item);
+		}   
+		return CCTouchDispatcher.kEventIgnored;
+	}
+
+	private boolean itemEvent(MotionEvent event, CCNode item) {
+		// Ignore Menu item, it take over by Menu
+		if (item instanceof CCMenuItem)
+			return CCTouchDispatcher.kEventIgnored;
+		
+		if (!item.isRunning())
+			return CCTouchDispatcher.kEventIgnored;
+	    
+		// Go through all children, reverse to draw order
+		
+	    if (item.getChildren() != null) {
+	    	for (int i=item.getChildren().size()-1; i>=0; --i) {
+	    		CCNode child = item.getChildren().get(i);
+	    		if (child.getZOrder() >= 0) {
+	    			if (itemEvent(event, child) == CCTouchDispatcher.kEventHandled)
+	    				return CCTouchDispatcher.kEventHandled;
+	    		} else
+	    			continue;
+	    	}
+	    }	
+	    
+	    // Ignore Layer who take over itself 
+	    if (!(item instanceof CCLayer && ((CCLayer)item).isTouchEnabled())) {
+	    	if (uiXYInNode(item, event.getX(), event.getY())) {
+	        	if(itemEventImpl(event, item) == CCTouchDispatcher.kEventHandled) {
+					return CCTouchDispatcher.kEventHandled;
+				}
+	    	}
+	    }
+
+	    if (item.getChildren() != null) {
+	    	for (int i=item.getChildren().size()-1; i>=0; --i) {
+	    		CCNode child = item.getChildren().get(i);
+	    		if (child.getZOrder() < 0) {
+	    			if (itemEvent(event, child) == CCTouchDispatcher.kEventHandled)
+	    				return CCTouchDispatcher.kEventHandled;
+	    		} else
+	    			continue;
+	    	}
+	    }
+	
+	    return CCTouchDispatcher.kEventIgnored;
+	}
+
+	@Override
+	final public boolean ccTouchesBegan(MotionEvent event) {
+	    return itemEvent(event, this);
+	}
+
+	@Override
+	final public boolean ccTouchesEnded(MotionEvent event) {
+		return itemEvent(event, this);
+	}
+
+	@Override
+	final public boolean ccTouchesCancelled(MotionEvent event) {
+		return itemEvent(event, this);
+	}
+
+	@Override
+	final public boolean ccTouchesMoved(MotionEvent event) {
+		return itemEvent(event, this);
+	}
+
+	public boolean ccTouchesBeganPro(MotionEvent event, CCNode item) {
+		return CCTouchDispatcher.kEventIgnored;
+	}
+
+	public boolean ccTouchesEndedPro(MotionEvent event, CCNode item) {
+		return CCTouchDispatcher.kEventIgnored;
+	}
+
+	public boolean ccTouchesCancelledPro(MotionEvent event, CCNode item) {
+		return CCTouchDispatcher.kEventIgnored;
+	}
+
+	public boolean ccTouchesMovedPro(MotionEvent event, CCNode item) {
+		return CCTouchDispatcher.kEventIgnored;
+	}
+
+}
